a254c5073a44bbd9b9af8279d559565a640e03e9
Xianzhong Zhou <xianzhong1985@163.com>
2018-04-16 23:06:00 +0800

    change the file that can find out the cutoff energy and the total energy, then plot the total energy vs. the cutoff energy

--- plot_convergence.py
+++ plot_convergence.py
@@ -2,11 +2,29 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-data=np.genfromtxt('energy.txt')
-plt.plot(data[:,0], data[:,1])
-plt.xlabel('ecutwfc (Ry) (ecutrho=8*ecutwfc)')
+cutoff=[]
+energy=[]
+for out_file in os.listdir('./'):
+    if out_file.find('out')>=0:
+        print(out_file)
+        with open(out_file) as f:
+            for i in f:
+                if i.find('kinetic-energy cutoff')>=0:
+                    cutoff.append(float(i.split()[3]))
+                if i.find('!')>=0:
+                    energy.append(float(i.split()[4]))
+
+dtype=[('cutoff',float),('energy',float)]
+cutoff=np.array(cutoff)
+energy=np.array(energy)
+energy=energy[np.argsort(cutoff)]
+cutoff=cutoff[np.argsort(cutoff)]
+plt.plot(cutoff, energy)
+plt.xlabel('ecutwfc (Ry) (ecutrho=4*ecutwfc)')
 plt.ylabel('Total Energy (Ry)')
 plt.tight_layout()
 plt.savefig('convergence.pdf')
-plt.show()
+for i in range(len(cutoff)):
+    print(cutoff[i], energy[i])
