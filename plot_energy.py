31113b2e3f407ef5603fff29f96447ec877fd37d
Xianzhong Zhou <xianzhong1985@163.com>
2018-08-21 20:25:55 +0800

    combine two file

--- plot_energy.py
+++ plot_energy.py
@@ -319,8 +319,38 @@ def plot_rashba(rashba_lattice):
             plt.savefig(prefix[j]+''+label_name+'.pdf')
             plt.close()
 
+def plot_convergence():
+    cutoff=[]
+    energy=[]
+    for out_file in os.listdir('./'):
+        if out_file.find('out')>=0:
+            print(out_file)
+            with open(out_file) as f:
+                for i in f:
+                    if i.find('kinetic-energy cutoff')>=0:
+                        cutoff.append(float(i.split()[3]))
+                    if i.find('!')>=0:
+                        energy.append(float(i.split()[4]))
+
+    dtype=[('cutoff',float),('energy',float)]
+    cutoff=np.array(cutoff)
+    energy=np.array(energy)
+    energy=energy[np.argsort(cutoff)]
+    cutoff=cutoff[np.argsort(cutoff)]
+    plt.plot(cutoff, energy)
+    plt.xlabel('ecutwfc (Ry)')
+    plt.ylabel('Total Energy (Ry)')
+    plt.tight_layout()
+    plt.savefig('convergence.pdf')
+    for i in range(len(cutoff)):
+        print(cutoff[i], energy[i])
+    np.savetxt('cutoff.txt',(cutoff, energy))
+
+
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument("out", choices=['step', 'lattice', 'band', 'rashba'], help="The kind of file want to plot")
+parser.add_argument("out", choices=['step','convergence', 'lattice', 'band', 'rashba'], help="The kind of file want to plot")
 args = parser.parse_args()
 
 if args.out == 'lattice':
@@ -330,6 +360,9 @@ if args.out == 'lattice':
     plot_lattice_energy(lattice, energy)
     np.savetxt('lattice.txt',(lattice, energy))
 
+
+if args.out == 'convergence':
+    plot_convergence()
 
 
 if args.out == 'band':
